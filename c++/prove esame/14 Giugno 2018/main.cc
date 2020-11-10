a67d02d52456b206cf541c0e3f676c876e4e919a
paoli7612 <paoli7612@gmail.com>
2020-11-10 11:35:45 +0100

    Maca solo scrivi e carica.. ma sembra andare

--- c++/prove esame/14 Giugno 2018/main.cc
+++ c++/prove esame/14 Giugno 2018/main.cc
@@ -2,6 +2,78 @@
 
 using namespace std;
 
+typedef char Nome;
+
+struct Pacchetto {
+	Nome nome;
+	Pacchetto* dipendente = NULL;
+};
+
+struct Dipendenze {
+	int N;
+	Pacchetto* pacchetti;
+	bool init = false;
+} dipendenze;
+
+Pacchetto* cerca(Nome nome){
+	for (int i=0; i<dipendenze.N; i++){
+		Pacchetto* p = &dipendenze.pacchetti[i];
+		if (p->nome == nome)
+			return p;
+	}
+	return &dipendenze.pacchetti[0];
+}
+
+Nome chiedi_pacchetto(Nome n = ('A'-1)){
+	Nome nome;
+	do {
+		cout << "Inserisci il nome del pacchetto(dalla A alla " << (char)(dipendenze.N + 'A' - 1);
+		if (n != ('A'-1))
+			cout << ", non " << n;
+		cout << "): ";
+		cin >> nome;
+	} while (nome < 'A' || nome > ('A' + dipendenze.N) || nome == n);
+	return nome;
+}
+
+void inizializza(int n){
+	dipendenze.N = n;
+	dipendenze.pacchetti = new Pacchetto[n];
+	dipendenze.init = true;
+	for (int i=0; i<n; i++)
+		dipendenze.pacchetti[i].nome = 'A' + i;
+}
+
+void imposta_dipendenza(Nome nome1, Nome nome2){
+	Pacchetto *a = cerca(nome1);
+	Pacchetto *b = cerca(nome2);
+	a->dipendente = b;
+}
+
+void stampa(){
+	for (int i=0; i<dipendenze.N; i++){
+		Pacchetto* p = &dipendenze.pacchetti[i];
+		cout << p->nome;
+		if (p->dipendente != NULL)
+			cout << " -> " << p->dipendente->nome;
+		cout << endl;
+	}
+}
+
+void stampa_catena(Pacchetto* current){
+	cout << current->nome;
+	if (current->dipendente != NULL){
+		cout << " -> ";
+		stampa_catena(current->dipendente);
+	}
+}
+
+void stampa_catena_delle_dipendenze(Nome start){
+	Pacchetto* p = cerca(start);
+	stampa_catena(p);
+	cout << endl;
+}
+
 int main(int argc, char** argv){
 	const char menu[] =
 		"1. Inizializza insieme\n"
@@ -20,23 +92,38 @@ int main(int argc, char** argv){
 		cin>>scelta;
 
 		switch (scelta) {
-			case 1:
+			case 1: // inizializza
+				int n;
+				cout << "Inserisci il numero di dipendenze dell'insieme: ";
+				cin >> n;
+				inizializza(n);
 				break;
-			case 2:
+			case 2: // imposta dipendenze
+				char n1, n2;
+				cout << "Inserisci il nome del pacchetto" << endl;
+				n1 = chiedi_pacchetto();
+				cout << "Inserisci il nome del pacchetto a cui farlo dipendere" << endl;
+				n2 = chiedi_pacchetto(n1);
+				imposta_dipendenza(n1, n2);
 				break;
-			case 3:
+			case 3: // stampa insieme
+				stampa();
 				break;
-			case 4:
+			case 4: // salva insieme
 				break;
-			case 5:
+			case 5: // carica insieme
 				break;
-			case 6:
+			case 6: // stampa catena delle dipendenze
+				Nome n3;
+				cout << "Inserisci il nome da cui partire" << endl;
+				n3 = chiedi_pacchetto();
+				stampa_catena_delle_dipendenze(n3);
 				break;
-			case 7:
+			case 7: // esci
 				running = false;
 				break;
 			default:
-				cout<<"Scelta errata"<<endl;
+				cout << "Scelta errata" << endl;
 		}
 	}
 	return 0;
