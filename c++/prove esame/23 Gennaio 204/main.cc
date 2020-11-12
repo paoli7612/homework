ad4563e625c69e5cea0acebbe1da286d7053a5c5
paoli7612 <paoli7612@gmail.com>
2020-11-12 12:27:00 +0100

    ok ma non si puo "eliminare" .. rifare

--- c++/prove esame/23 Gennaio 204/main.cc
+++ c++/prove esame/23 Gennaio 204/main.cc
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 const int N = 10;
-const int M = 5;
+const int M = 30;
 
 typedef char* Codice;
+
+bool equals(Codice a, Codice b){
+    if (strlen(a) != strlen(b))
+        return false;
+    for (int i=0; i<strlen(a); i++)
+        if (a[i] != b[i])
+            return false;
+    return true;
+}
+
+int ask(const char text[]){
+    cout << text << ": ";
+    int a;
+    cin >> a;
+    return a;
+}
+
+char* ask_codice(){
+    cout << "Codice: ";
+    Codice codice = new char[M];
+    cin >> codice;
+    codice[strlen(codice)] = '-';
+    return codice;
+}
 
 struct Pos {
     int scaffale;
@@ -61,41 +86,68 @@ void stampa_prodotti(){
         stampa_prodotto(magazzino.prodotti[i]);
 }
 
-/* Elimina tutti i prodotti di codice cod. Si ottiene il punteggio
-massimo se si realizza questa funzionalità a costo O(N) (anche se questo comporta la creazione
-di oggetti temporanei e la loro successiva eliminazione). */
+/* Elimina tutti i prodotti di codice cod */
 void elimina_codice_prodotto(Codice codice){
+    for (int i=0; i<magazzino.T; i++){
+        Prodotto* p = &magazzino.prodotti[i];
+        if (equals(p->codice, codice)){
+            // Elimina prodotto
+            cout << "Elimina prodotto" << endl;
+        }
+    }
+
+}
+/* Salva il contenuto del magazzino in un file di testo */
+void salva_prodotti(){
+
+}
+
+/* Carica il contenuto del magazzino dal file. Il precedente contenuto è perso */
+void carica_prodotti(){
 
 }
 
 
 int main(int argc, char** argv){
-    Codice codice = new char[M];
-    Pos pos;
-    pos.ripiano = 1;
-    pos.scaffale = 2;
-    codice = "asd";
-    inserisci_prodotto(codice, pos);
-    stampa_prodotti();
-
+    const char menu[] = 
+        "1. Leggi codice promozione\n"
+        "2. Inserisci prodotto\n"
+        "3. Stampa prodotti\n"
+        "4. Salva prodotti\n"
+        "5. Carica prodotti\n"
+        "6. Elimina codice prodotto\n"
+        "7. Esci\n";
 
     bool running = true;
     while (running){
 
+        cout << menu;
+
         int scelta;
         cin >> scelta;
 
+        Codice codice;
+        Pos posizione;
+
         switch (scelta){
             case 1: // Leggi codice posizione
-                Codice codice = new char[M];
-                cout << "Codice: ";
-                cin >> codice;
+                codice = ask_codice();
+                posizione.scaffale = ask("Scaffale");
+                posizione.ripiano = ask("Ripiano");
+                inserisci_prodotto(codice, posizione);
                 break;
             case 2: // Inserisci prodotto       
                 break;
             case 3: // Stampa prodotti
+                stampa_prodotti();
+                break;
+            case 4: // Salva prodotti
+                break;
+            case 5: // Carica prodotti
                 break;
             case 6: // Elimina codice prodotto
+                codice = ask_codice();
+                elimina_codice_prodotto(codice); 
                 break;
             case 7: // Esci
                 running = false;
