aef5f2d0e326c9b753a206b23ffaaa0c41c97b76
Tommaso Paoli <paoli7612@gmail.com>
2021-01-13 20:49:03 +0100

    noskypsw

---
+++ c++/Esercitazione 7/car_codice.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// noskypsw
+
+int main(int argc, char** argv){
+	
+	
+	for (int i=0; i<10; i++){
+		char a;
+		cin >> noskipws >> a;
+		cout << a << endl;
+	}
+	
+	return 0;
+}
--- c++/Esercitazione 7/mostra_visibilita.cc
+++ c++/Esercitazione 7/mostra_visibilita.cc
@@ -1,10 +1,9 @@
 #include <iostream>
 
-// Boring
-
 using namespace std;
 
 int main(int argc, char** argv){
+	
 	
 	return 0;
 }
