71e08594f891322e7f8bc23c81e23c339b3bf101
Tommaso Paoli <paoli7612@gmail.com>
2021-01-14 21:22:43 +0100

    Step 1

--- c++/prove esame/Appello 1 2021/main.cc
+++ c++/prove esame/Appello 1 2021/main.cc
@@ -1,90 +1,70 @@
-// Appello 1
-
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
+const char FILENAME[] = "data.txt";
+
 struct coda_t {
-	char* vet; // Puntatore a char (array dinamico)
-	int N; // lunghezza del vettore
-	int s; // indice del vettore da cui comincia la coda
-	int u; // numero di utenti in coda
+	char* vettore;
+	int N;
+	int start, el;
 };
 
-// Inizializza coda vuota e lunghezza=0
-void inizializza(struct coda_t &c){
-	c.vet = new char[0];
+void init(struct coda_t &c){
+	c.vettore = new char[0];
 	c.N = 0;
-	c.N = 0;
+	c.start = 0;
+	c.el = 0;
 }
 
-bool inizializza_coda(struct coda_t &c, int n){
-	if (n < 0) // lunghezza coda negativa ?!?!?
+char& get(const struct coda_t &c, int i){
+	return c.vettore[(i+c.start)%c.N];
+}
+
+bool inizializza_coda(struct coda_t &c, const int N){
+	if (N < 0) // Inizialzzo a lunghezza negativa?
 		return false;
-	
-	c.N = n;
-	delete[] c.vet; // dealloca la precedente coda
-	c.vet = new char[n];
-	c.s = 0;
+
+	c.vettore = new char[N];
+	c.N = N;
+	c.start = 0;
+	c.el = 0;
 }
 
-// accoda utente codice u
-bool accoda_utente(struct coda_t &c, char u){
-	if (c.N == c.u) // Coda piena
-		return false;
-	
-	c.vet[(c.s+(c.u++))%c.N] = u;
+void stampa(const struct coda_t &c, ostream out=cout, bool is_file=true){
+	if (is_file)
+		out << c.N << " " << c.N << endl;
+	for (int i=0; i<c.el; i++){
+		out << (char)get(c, i) << " ";
+	}
 }
 
-void stampa_coda(const struct coda_t &c, ostream &out=cout, bool isfile=false){
-	if (isfile)
-		out << c.N << " " << c.u << endl;
-	
-	for (int i=c.s; i<c.s+c.u; i++)
-		cout << c.vet[i%c.N] << " ";
-	
-	out << endl;
-}
-
-void scambia(char &a, char &b){
-	char t = a;
-	a = b;
-	b = t;
-}
-
-void anticipa_servizio(struct coda_t &c, char s){
-
-}
-
-bool servi_prossimo_utente(struct coda_t &c){
-	if (c.u == 0) // Nessun utente in coda
+bool accoda_utente(struct coda_t &c, char s){
+	if (c.el == c.N) // Coda piena
 		return false;
 		
-	c.s = (c.s+1)%c.N;
-	c.u--;
+	c.vettore[c.el++%c.N] = s;
+}
+
+void salva_coda(const struct coda_t &c){
+	ofstream file(FILENAME);
+	stampa(c, file, true);
+	file.close();
 }
 
 int main(){
 	
 	coda_t c;
-	inizializza(c);
+	init(c);
 	inizializza_coda(c, 10);
 	accoda_utente(c, 'A');
-	accoda_utente(c, 'C');
-	accoda_utente(c, 'D');
+	accoda_utente(c, 'B');
 	accoda_utente(c, 'A');
 	accoda_utente(c, 'A');
-	accoda_utente(c, 'C');
-	accoda_utente(c, 'D');
-	accoda_utente(c, 'A');
-	stampa_coda(c);
 	
-	servi_prossimo_utente(c);
-	stampa_coda(c);
-	
-	anticipa_servizio(c, 'A');
-	stampa_coda(c);
+	stampa(c);
+	//salva_coda(c);
 	
 	return 0;
 }
