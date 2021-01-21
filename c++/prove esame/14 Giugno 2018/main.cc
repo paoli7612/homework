3db8ada7bce8e209f6879ac6d9941903094b58d3
root <root@DESKTOP-CR0CR6R.localdomain>
2021-01-21 20:31:12 +0100

    14 giunio 2018

--- c++/prove esame/14 Giugno 2018/main.cc
+++ c++/prove esame/14 Giugno 2018/main.cc
@@ -1,89 +1,124 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 typedef char Nome;
 
-struct Pacchetto {
-	Nome nome;
-	Pacchetto* dipendente = NULL;
+const char FILENAME[] = "insieme.dat";
+
+const char MAX_PACCHETTI = 'z'-'A'+1;
+
+struct insieme_t {
+	int N;
+	int *dipendenze;
 };
 
-struct Dipendenze {
-	int N;
-	Pacchetto* pacchetti;
-	bool init = false;
-} dipendenze;
-
-Pacchetto* cerca(Nome nome){
-	for (int i=0; i<dipendenze.N; i++){
-		Pacchetto* p = &dipendenze.pacchetti[i];
-		if (p->nome == nome)
-			return p;
-	}
-	return &dipendenze.pacchetti[0];
+char nome(int i){
+	return static_cast<char>(i + 'A');
 }
 
-Nome chiedi_pacchetto(Nome n = ('A'-1)){
-	Nome nome;
-	do {
-		cout << "Inserisci il nome del pacchetto(dalla A alla " << (char)(dipendenze.N + 'A' - 1);
-		if (n != ('A'-1))
-			cout << ", non " << n;
-		cout << "): ";
-		cin >> nome;
-	} while (nome < 'A' || nome > ('A' + dipendenze.N) || nome == n);
-	return nome;
+void init(insieme_t &insieme)
+{
+	insieme.N = 0;
+	insieme.dipendenze = new int[0];
 }
 
-void inizializza(int n){
-	dipendenze.N = n;
-	dipendenze.pacchetti = new Pacchetto[n];
-	dipendenze.init = true;
-	for (int i=0; i<n; i++)
-		dipendenze.pacchetti[i].nome = 'A' + i;
+bool reinizializza_insieme(insieme_t &insieme, const int N)
+{
+	if (N < 0)
+		return false;
+
+	delete [] insieme.dipendenze;
+	insieme.dipendenze = new int[N];
+
+	for (int i=0; i<N; i++)
+		insieme.dipendenze[i] = MAX_PACCHETTI;
+		
+	insieme.N = N;
+
+	return true;
 }
 
-void imposta_dipendenza(Nome nome1, Nome nome2){
-	Pacchetto *a = cerca(nome1);
-	Pacchetto *b = cerca(nome2);
-	
-	Pacchetto *t = b;
-	do {
-		if (nome1 == t->nome){
-			cout << "Si creerebbe un loop" << endl;
-			return;
-		}
-		t = t->dipendente;
-	} while (t != NULL);
-	a->dipendente = b;
+bool imposta_dipendenza(insieme_t &insieme, const int p1, const int p2)
+{
+	if (p1 < 0 || p2 < 0 || p1 >= insieme.N || p2 >= insieme.N)
+		return false;
+
+	insieme.dipendenze[p1] = p2;
+	return true;
 }
 
-void stampa(){
-	for (int i=0; i<dipendenze.N; i++){
-		Pacchetto* p = &dipendenze.pacchetti[i];
-		cout << p->nome;
-		if (p->dipendente != NULL)
-			cout << " -> " << p->dipendente->nome;
+bool imposta_dipendenza(insieme_t &insieme, const char p1, const char p2)
+{
+	return imposta_dipendenza(insieme, nome(p1), nome(p2));
+}
+
+void stampa_dipendenze(const insieme_t &insieme)
+{
+	for (int i=0; i<insieme.N; i++)
+	{
+		cout << nome(i);
+		if (insieme.dipendenze[i] != MAX_PACCHETTI)
+			cout << " -> " << nome(insieme.dipendenze[i]);
 		cout << endl;
 	}
 }
 
-void stampa_catena(Pacchetto* current){
-	cout << current->nome;
-	if (current->dipendente != NULL){
+bool salva(const insieme_t &insieme)
+{
+	ofstream file(FILENAME);
+
+	if (!file)
+		return false;
+
+	file.write((char*)&insieme.N, sizeof(int));
+
+	for (short int i=0; i<insieme.N; i++)
+		file.write((char*)&insieme.dipendenze[i], sizeof(int));
+
+	file.close();
+	return static_cast<bool>(file);
+}
+
+bool carica(insieme_t &insieme)
+{
+	ifstream file(FILENAME);
+
+	if (!file)
+		return false;
+
+	int N;
+	file.read((char*)&N, sizeof(int));
+
+	reinizializza_insieme(insieme, N);
+
+	for (int i=0; i<N; i++)
+		file.read((char*)&insieme.dipendenze[i], sizeof(int));
+
+	file.close();
+	return static_cast<bool>(file);
+}
+
+void stampa_catena_dipendenze(insieme_t &insieme, int p, int n, bool first=true)
+{
+	if (n == -1)
+		return;
+
+	if (!first)
 		cout << " -> ";
-		stampa_catena(current->dipendente);
-	}
+
+	cout << nome(p);
+
+	if (insieme.dipendenze[p] != MAX_PACCHETTI)
+		stampa_catena_dipendenze(insieme, insieme.dipendenze[p], n-1, false);
 }
 
-void stampa_catena_delle_dipendenze(Nome start){
-	Pacchetto* p = cerca(start);
-	stampa_catena(p);
-	cout << endl;
-}
+int main(int argc, char** argv)
+{
+	insieme_t insieme;
+	init(insieme);
 
-int main(int argc, char** argv){
 	const char menu[] =
 		"1. Inizializza insieme\n"
 		"2. Imposta dipendenza\n"
@@ -98,40 +133,51 @@ int main(int argc, char** argv){
 		cout<<menu<<endl;
 
 		int scelta;
-		cin>>scelta;
-
-		if (scelta > 1 && scelta < 7 && !dipendenze.init && scelta != 4){
-            cout << "Insieme non inizializato" << endl;
-            continue;
-        }
+		cin >> scelta;
 
 		switch (scelta) {
 			case 1: // inizializza
-				int n;
-				cout << "Inserisci il numero di dipendenze dell'insieme: ";
-				cin >> n;
-				inizializza(n);
+			{
+				int N;
+				cout << "N: ";
+				cin >> N;
+
+				reinizializza_insieme(insieme, N);
+			}
 				break;
 			case 2: // imposta dipendenze
-				char n1, n2;
-				cout << "Inserisci il nome del pacchetto" << endl;
-				n1 = chiedi_pacchetto();
-				cout << "Inserisci il nome del pacchetto a cui farlo dipendere" << endl;
-				n2 = chiedi_pacchetto(n1);
-				imposta_dipendenza(n1, n2);
+			{
+				int p1, p2;
+				cout << "Inserisci l'indice del pacchetto (p1): ";
+				cin >> p1;
+				cout << "Inserisci l'indice del pacchetto (p2): ";
+				cin >> p2;
+
+				imposta_dipendenza(insieme, p1, p2);
+			}
 				break;
 			case 3: // stampa insieme
-				stampa();
+				stampa_dipendenze(insieme);
 				break;
 			case 4: // salva insieme
+				if (!salva(insieme))
+					cout << "Errore salvataggio" << endl;
 				break;
 			case 5: // carica insieme
+				if (!carica(insieme))
+					cout << "Errore caricamento" << endl;
 				break;
 			case 6: // stampa catena delle dipendenze
-				Nome n3;
-				cout << "Inserisci il nome da cui partire" << endl;
-				n3 = chiedi_pacchetto();
-				stampa_catena_delle_dipendenze(n3);
+			{
+				int n, p;
+				cout << "Inserisci l'indice del primo pacchetto (p): ";
+				cin >> p;
+				cout << "Inserisci il numero massimo di dipendenze (n): ";
+				cin >> n;
+
+				stampa_catena_dipendenze(insieme, p, n);
+				cout << endl;
+			}
 				break;
 			case 7: // esci
 				running = false;
