644f65c4681d5bf9b0023bc82d2106e08bd94343
Tommaso Paoli <paoli7612@gmail.com>
2021-02-12 22:28:53 +0100

    run

---
+++ c++/Programmazione 2/2/main.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+
+using namespace std;
+
+#include "grafo.h"
+
+int main(int argc, char **argv)
+{
+  graph_t grafo;
+  init(grafo, 10);
+
+  system("pause");
+
+  return 0;
+}
